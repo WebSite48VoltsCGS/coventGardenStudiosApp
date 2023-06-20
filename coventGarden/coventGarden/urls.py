140ab7d77740c1191542c49c1430a72707383b38
harolinho <haroldkengne@gmail.com>
2023-06-20 17:21:18 +0200

    initial commit

---
+++ coventGarden/coventGarden/urls.py
@@ -0,0 +1,34 @@
+"""coventGarden URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from studios import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('accueil/', views.home, name='home'),
+
+    path('actualites/', views.news, name='news'),
+    path('studios/', views.studios, name='studios'),
+    path('bar/', views.bar, name='bar'),
+    path('espace_pro/', views.pro_area, name='pro_area'),
+    path('contact/', views.contact, name='contact'),
+    path('réservation/', views.booking, name='booking'),
+    path('compte/', views.account, name='account'),
+    path('se_connecter/', views.sign_in, name='sign_in'),
+    path('créer_un_compte/', views.sign_up, name='sign_up'),
+]
---
+++ coventGarden/studios/templates/studios/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.core.mail import send_mail
+
+def home(request):
+    return render(request, 'studios/home.html')
+
+def news(request):
+    return render(request, 'studios/news.html')
+
+def studios(request):
+    return render(request, 'studios/studios.html')
+
+def bar(request):
+    return render(request, 'studios/bar.html')
+
+def pro_area(request):
+    return render(request, 'studios/pro_area.html')
+
+def contact(request):
+    return render(request, 'studios/contact.html')
+
+def booking(request):
+    return render(request, 'studios/booking.html')
+
+def account(request):
+    return render(request, 'studios/account.html')
+
+def sign_in(request):
+    return render(request, 'studios/signin.html')
+
+def sign_up(request):
+    return render(request, 'studios/base/signup.html')
+
+
+
