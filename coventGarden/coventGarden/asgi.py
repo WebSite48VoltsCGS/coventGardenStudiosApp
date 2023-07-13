8aa10d343c3fef098d36330ebf4c4197c9409c71
harolinho <haroldkengne@gmail.com>
2023-07-13 06:45:57 +0200

    version 1.2.0 de coventgarden

--- coventGarden/coventGarden/asgi.py
+++ coventGarden/coventGarden/asgi.py
@@ -4,7 +4,11 @@ ASGI config for coventGarden project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+<<<<<<< HEAD
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+=======
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+>>>>>>> 52f8c78af420c12d762aac510dbc4755b9199d2a
 """
 
 import os
