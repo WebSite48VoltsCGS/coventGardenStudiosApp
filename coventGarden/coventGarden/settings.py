7ee220e1bd5c4529ab4612b1b63ec63cb6927871
harolinho <haroldkengne@gmail.com>
2023-07-13 06:24:34 +0200

    dump de la BD

--- coventGarden/coventGarden/settings.py
+++ coventGarden/coventGarden/settings.py
@@ -88,22 +88,32 @@ WSGI_APPLICATION = 'coventGarden.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'coventgarden',
-#        'USER': 'admin',
-#        'PASSWORD': 'admin123',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql_psycopg2",
+#        "NAME": "coventgarden",
+#        "HOST": "localhost",
+#        "PORT": "5432",
+#    }
+#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coventgarden',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
